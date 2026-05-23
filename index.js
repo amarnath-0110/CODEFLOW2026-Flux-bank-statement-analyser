9f9a54999917bf094d709e4660165a640d8370ea
amarnath-0110 <amarnathkumar012006@gmail.com>
2026-05-23 14:56:05 +0530

    Initialising backend

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import bodyParser from "body-parser";
+const app = express();
+const port = 3000;
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
